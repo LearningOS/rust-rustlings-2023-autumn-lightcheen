5f3ff795214639246b6aa1217c614b1819ba7d6e
lightcheen <light1000000111@163.com>
2023-10-27 23:13:41 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,40 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut begin = 0;
+    let mut end = 0;
+    let mut judge = false;
+    let mut count=0;
+    for c in input.chars() {
+        if c != ' ' && judge == false{
+            begin = count;
+            judge = true;
+        }
+        if c != ' ' && judge == true{
+            end = count;
+        }
+        count =count + 1;
+    }
+    let s = &input[begin..end+1];
+    return s.to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut input2 = String::from("");
+    input2.push_str(input);
+    input2.push_str(" world!");
+    return input2;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut input2 = String::from("");
+    input2.push_str(input);
+    input2.replace("cars","balloons")
 }
 
 #[cfg(test)]
